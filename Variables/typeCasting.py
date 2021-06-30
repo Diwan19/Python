cc4c6baedb1fbce45522b66cc774d03f958ce8f4
Himanshu_Diwan <31444033+Diwan19@users.noreply.github.com>
2021-06-30 13:43:21 +0530

    Type casting in python

---
+++ Variables/typeCasting.py
@@ -0,0 +1,19 @@
+# type casting in python
+# python used classes to define data types
+
+a = int(2)
+b = int(33.3)
+c = int("8")
+
+print(a,b,c)
+
+x = str("st2")
+y = str(3)
+z = str(3.3)
+
+print(x,y,z)
+
+u = float(3)
+t = float("4.4")
+
+print(u,t)
